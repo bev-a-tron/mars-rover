34d5025c79d9771a281bff718165e0f6de439c8c
Beverly Lau <beverly.a.lau@gmail.com>
2014-05-07 14:10:45 -0700

    Refactor | Beverly | remove position attribute from MarsRover

--- src/swordfish/MarsRover.java
+++ src/swordfish/MarsRover.java
@@ -2,13 +2,11 @@ package swordfish;
 
 public class MarsRover {
 
-    private int[] position;
     private String direction;
     private Point point;
 
     public MarsRover(Point point, String direction) {
         this.point = point;
-        this.position = new int[]{point.x, point.y};
         this.direction = direction;
     }
 
@@ -33,25 +31,21 @@ public class MarsRover {
     public void move() {
 
         if (direction.equals("N")) {
-            position[1] ++;
 
             Point directionAsPoint = new Point(0, 1);
             point.plus(directionAsPoint);
 
         } else if (direction.equals("S")) {
-            position[1] --;
 
             Point directionAsPoint = new Point(0, -1);
             point.plus(directionAsPoint);
 
         } else if (direction.equals("E")) {
-            position[0] ++;
 
             Point directionAsPoint = new Point(1, 0);
             point.plus(directionAsPoint);
 
         } else if (direction.equals("W")) {
-            position[0] --;
 
             Point directionAsPoint = new Point(-1, 0);
             point.plus(directionAsPoint);
