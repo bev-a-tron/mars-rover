6d762dadb7ed975318d43917976eeacb3098592a
Beverly Lau <beverly.a.lau@gmail.com>
2014-05-19 15:29:27 -0700

    Refactor | Beverly | change Main to iterate over solutions and print

--- src/swordfish/Main.java
+++ src/swordfish/Main.java
@@ -6,13 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         String data = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM";
         App app = new App(data);
 
         List<String> finalPosition = app.start();
 
-        System.out.println(finalPosition.get(0));
+        for (String item : finalPosition) {
+            System.out.println(item);
+        }
     }
 
 }
